c88b02203d5d83e6d00a71087c745af660c6cefb
Erick Tucto <erick@ericktucto.com>
2025-01-16 09:06:54 -0500

    change: remove to coc config and formatter config (conform.nvim)

--- lua/stixcode/lsp.lua
+++ lua/stixcode/lsp.lua
@@ -1,8 +1,8 @@
 local lsp_zero = require("lsp-zero")
 local cmp = require("cmp")
 local defaultOptions = require("stixcode.configs.lsp")
-lsp_zero.on_attach(function(_, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
+lsp_zero.on_attach(function (_, bufnr)
+  lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 require("lspconfig").phpactor.setup({})
 require("lspconfig").bashls.setup({})
@@ -10,8 +10,8 @@ require("lspconfig").bashls.setup({})
 require("mason").setup({})
 require("mason-lspconfig").setup({
   handlers = {
-    function(server_name)
-      local options  = {}
+    function (server_name)
+      local options = {}
 
       if defaultOptions[server_name] ~= nil then
         options = defaultOptions[server_name]
@@ -25,8 +25,12 @@ require("codeium").setup()
 cmp.setup({
   sources = {
     { name = "nvim_lsp" },
-    { name = "luasnip", keyword_length = 2 },
     { name = "codeium" },
+  },
+  snippet = {
+    expand = function (args)
+      vim.snippet.expand(args.body)
+    end
   },
   window = {
     completion = cmp.config.window.bordered(),
@@ -44,25 +48,25 @@ cmp.setup({
   }),
 })
 vim.api.nvim_create_autocmd({ "CursorHold" }, {
-    pattern = "*",
-    callback = function()
-        for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
-            if vim.api.nvim_win_get_config(winid).zindex then
-                return
-            end
-        end
-        vim.diagnostic.open_float({
-            scope = "cursor",
-            focusable = false,
-            close_events = {
-                "CursorMoved",
-                "CursorMovedI",
-                "BufHidden",
-                "InsertCharPre",
-                "WinLeave",
-            },
-        })
+  pattern = "*",
+  callback = function ()
+    for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+      if vim.api.nvim_win_get_config(winid).zindex then
+        return
+      end
     end
+    vim.diagnostic.open_float({
+      scope = "cursor",
+      focusable = false,
+      close_events = {
+        "CursorMoved",
+        "CursorMovedI",
+        "BufHidden",
+        "InsertCharPre",
+        "WinLeave",
+      },
+    })
+  end
 })
 -- disable diagnostic inline
 vim.diagnostic.config {
